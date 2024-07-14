87f098b3af08b83f9d4a8ade13b6c438257c9cef
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2024-07-14 15:41:04 +0300

    feat: add test-extension for testing

---
+++ extension-files/test-extension/src/contentScript.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Log `title` of current active web page
+const pageTitle = document.head.getElementsByTagName('title')[0].innerHTML;
+console.log(
+  `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
+);
+
+const injectIFrame = () => {
+  const iframe = document.createElement('iframe');
+  iframe.id = "bringweb3-extension";
+  iframe.src = "http://localhost:5173/";
+  iframe.style.width = "350px";
+  iframe.style.height = "600px";
+  iframe.style.position = "fixed";
+  iframe.style.right = "14px";
+  iframe.style.top = "32px";
+  iframe.style.zIndex = "9999";
+  iframe.style.borderRadius = "10px";
+  iframe.style.border = "1px solid #ccc";
+  iframe.sandbox = "allow-popups allow-scripts allow-same-origin";
+  document.body.insertBefore(iframe, document.body.firstChild);
+}
+
+
+// Listen for message
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'INJECT') {
+    console.log(`injecting to: `, request.domain);
+    injectIFrame();
+  }
+  sendResponse({});
+  return true;
+});
--- iframe-frontend/src/App.tsx
+++ iframe-frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import style from './app.module.css'
+
 const App = () => {
 
   return (
-    <>
-      <h1>BRINGWEB3</h1>
-    </>
+    <div className={style.container}>
+      <h1 className={style.h1}>BRINGWEB3</h1>
+      <button className={style.btn}>Activate</button>
+    </div>
   )
 }
 
