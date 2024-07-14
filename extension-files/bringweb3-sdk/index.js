fe45acf7927afd68013660320b80ce541bffc1c1
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2024-07-14 18:37:29 +0300

    fix: missed file changes

--- extension-files/bringweb3-sdk/index.js
+++ extension-files/bringweb3-sdk/index.js
@@ -1,9 +1,38 @@
-const fetchRetailers = async () => {
-    const res = fetch('http://localhost:3001')
-    const json = await res.json()
-    console.log(json);
+import { fetchRetailers } from "../../../bringweb3-sdk/utils/api"
+import storage from "../../../bringweb3-sdk/utils/storage"
+
+const updateCache = async () => {
+    const retailers = await fetchRetailers()
+    storage.set('relevantDomains', retailers.relevantDomains)
 }
 
-module.exports = {
-    fetchRetailers
+const getDomain = (url) => {
+    const urlInstance = new URL(url)
+    const domain = urlInstance.origin.split('://')[1].replace('www.', '')
+    return domain
+}
+
+const initialize = async () => {
+    updateCache()
+
+    chrome.tabs.onUpdated.addListener(async function (tabId, info, tab) {
+        if (info.status === 'complete') {
+            const relevantDomains = await storage.get('relevantDomains')
+
+            const domain = getDomain(tab.url)
+
+            if (relevantDomains.includes(domain)) {
+                chrome.tabs.sendMessage(tabId, {
+                    type: 'INJECT',
+                    domain
+                });
+            }
+        }
+    })
+
+    setInterval(async () => updateCache, 1000 * 60);
+}
+
+export default {
+    initialize
 }
