006224ddf68a902c821403f043997d06e3f1a6fb
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2024-09-01 16:27:03 +0300

    fix: storage API compitabillity for manifest V2

--- extension-files/bringweb3-sdk/utils/storage.ts
+++ extension-files/bringweb3-sdk/utils/storage.ts
@@ -1,20 +1,51 @@
+//All functions are written so they would be compatible with both Manifest V2 and V3.
+
 const set = async (key: string, value: any) => {
-    chrome.storage.local.set({ [`bring_${key}`]: value });
+    return new Promise<void>((resolve, reject) => {
+        chrome.storage.local.set({ [`bring_${key}`]: value }, () => {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } else {
+                resolve();
+            }
+        });
+    });
 }
 
 const get = async (key: string) => {
-    const data = await chrome.storage.local.get(`bring_${key}`)
-    return data[`bring_${key}`]
+    return new Promise<any>((resolve, reject) => {
+        chrome.storage.local.get([`bring_${key}`], (data) => {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } else {
+                resolve(data[`bring_${key}`]);
+            }
+        });
+    });
 }
 
 const remove = async (key: string) => {
-    chrome.storage.local.remove(`bring_${key}`)
+    return new Promise<void>((resolve, reject) => {
+        chrome.storage.local.remove([`bring_${key}`], () => {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } else {
+                resolve();
+            }
+        });
+    });
 }
 
 // const clear = async () => {
-//     chrome.storage.local.clear(() => {
-//         console.log('Cache cleared successfully')
-//     })
+//     return new Promise<void>((resolve, reject) => {
+//         chrome.storage.local.clear(() => {
+//             if (chrome.runtime.lastError) {
+//                 reject(chrome.runtime.lastError);
+//             } else {
+//                 resolve();
+//             }
+//         });
+//     });
 // }
 
 export default {
