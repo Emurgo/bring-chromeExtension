a6b3d7d939ab5618882fa1c0d3fc38537b687a0e
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2024-12-27 11:54:34 +0200

    fix: navigating inside the same website

--- extension-files/test-extension/src/background.js
+++ extension-files/test-extension/src/background.js
@@ -3,6 +3,6 @@ import { bringInitBackground } from '@bringweb3/chrome-extension-kit'
 
 bringInitBackground({
     identifier: process.env.PLATFORM_IDENTIFIER,
-    apiEndpoint: 'sandbox', // 'sandbox' || 'prod'
+    apiEndpoint: 'prod', // 'sandbox' || 'prod'
     cashbackPagePath: '/main_window.html#/cashback'
 })
