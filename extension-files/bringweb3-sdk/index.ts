ba1e74d0406c5bff10a3558173e0b6970f910658
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2025-05-11 11:50:24 +0300

    fix: github workflow

--- extension-files/bringweb3-sdk/index.ts
+++ extension-files/bringweb3-sdk/index.ts
@@ -2,7 +2,7 @@ import bringInitContentScript from "./bringInitContentScript.js"
 import bringInitBackground from "./bringInitBackground.js"
 import { getTurnOff, setTurnOff } from './turnOffSettings.js'
 import { getPopupEnabled, setPopupEnabled } from './enablePopupSettings.js'
-// 
+
 export {
     bringInitBackground,
     bringInitContentScript,
