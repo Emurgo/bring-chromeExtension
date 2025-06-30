7e7995276ca1394c8c309fa01813d2c828fa14fa
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2025-06-30 17:23:25 +0300

    fix: handle decodeURIComponent failed

--- iframe-frontend/src/hooks/useCustomTheme.ts
+++ iframe-frontend/src/hooks/useCustomTheme.ts
@@ -23,7 +23,13 @@ const useCustomTheme = (): ThemeParamsResult => {
         // Apply custom theme
         Object.entries(theme).map(([key, value]) => {
             if (themeNames[key]) {
-                document.documentElement.style.setProperty(themeNames[key], decodeURIComponent(value))
+                let val = ''
+                try {
+                    val = decodeURIComponent(value)
+                } catch (error) {
+                    val = value
+                }
+                document.documentElement.style.setProperty(themeNames[key], val)
             }
         })
         setDone(true)
