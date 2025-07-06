4b59f15d8a7b3dd2ef77d2212e6e704c06da1d34
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2025-07-06 15:51:30 +0300

    fix: change opt out to specific

--- iframe-frontend/src/components/Offer/Offer.tsx
+++ iframe-frontend/src/components/Offer/Offer.tsx
@@ -4,7 +4,6 @@ import { useCallback, useEffect, useState } from 'react'
 import { useGoogleAnalytics } from '../../hooks/useGoogleAnalytics'
 import OptOut from '../OptOut/OptOut'
 import activate from '../../api/activate'
-import CryptoSymbolSelect from '../CryptoSymbolSelect/CryptoSymbolSelect'
 import CloseBtn from '../CloseBtn/CloseBtn'
 import PlatformLogo from '../PlatformLogo/PlatformLogo'
 import SwitchBtn from '../SwitchBtn/SwitchBtn'
@@ -15,6 +14,8 @@ import { useRouteLoaderData } from 'react-router-dom'
 import toCaseString from '../../utils/toCaseString'
 import { useWalletAddress } from '../../hooks/useWalletAddress'
 import formatCashback from '../../utils/formatCashback'
+import toCapital from '../../utils/toCapital'
+import { motion, AnimatePresence } from 'framer-motion'
 
 interface BringEventData {
     from: string
@@ -29,11 +30,11 @@ interface Props {
     setRedirectUrl: (url: string) => void
 }
 
+
 const Offer = ({ info, nextFn, setRedirectUrl, closeFn }: Props) => {
     const { sendGaEvent } = useGoogleAnalytics()
     const { walletAddress, setWalletAddress } = useWalletAddress()
-    const { iconsPath, textMode, flowId, iconUrl, name } = useRouteLoaderData('root') as LoaderData
-    const [tokenSymbol, setTokenSymbol] = useState(info.cryptoSymbols[0])
+    const { textMode, flowId, iconUrl, name } = useRouteLoaderData('root') as LoaderData
     const [optOutOpen, setOptOutOpen] = useState(false)
     const [isAddressUpdated, setIsAddressUpdated] = useState(false)
     const [status, setStatus] = useState<'idle' | 'waiting' | 'switch' | 'activating' | 'done'>('idle')
@@ -52,7 +53,7 @@ const Offer = ({ info, nextFn, setRedirectUrl, closeFn }: Props) => {
                 platformName,
                 retailerId,
                 url,
-                tokenSymbol,
+                tokenSymbol: info.cryptoSymbols[0],
                 flowId
             })
             if (res.status !== 200) throw `Got ${res.status} status`
@@ -62,7 +63,7 @@ const Offer = ({ info, nextFn, setRedirectUrl, closeFn }: Props) => {
         } catch (error) {
             console.error(error);
         }
-    }, [info, walletAddress, tokenSymbol, flowId, setRedirectUrl, nextFn])
+    }, [info, walletAddress, flowId, setRedirectUrl, nextFn])
 
     const handleFirstClick = () => {
         setStatus('activating')
@@ -103,95 +104,118 @@ const Offer = ({ info, nextFn, setRedirectUrl, closeFn }: Props) => {
     }, [status, activateAction, info, walletAddressUpdate])
 
     return (
-        <div className={styles.container}>
+        <>
             <CloseBtn />
-            {walletAddress ?
-                <div className={styles.top_container}>
-                    <div className={styles.wallet_container}>
-                        <span className={styles.wallet}>{splitWordMaxFive(walletAddress)}</span>
-                    </div>
-                    <SwitchBtn
-                        callback={() => setStatus('switch')}
-                    />
-                </div>
-                :
-                <div className={styles.wallet_spacer} />
-            }
-            <div className={styles.details}>
-                <div style={{ display: 'flex', gap: '8px', alignItems: 'start' }}>
-                    <div className={styles.logo_container} >
-                        <div className={styles.logo_wrapper} >
-                            <img
-                                src={iconUrl}
-                                className={styles.logo}
-                                alt={`${name}-website-icon`}
-                                width={76}
-                                height={76}
-                            />
-                        </div>
-                        <div className={styles.logo_text}>{name}</div>
-                    </div>
-                    <img src="/icons/plus-sign.svg" alt="plus-sign" style={{ marginTop: '32px' }} />
-                    <div className={styles.logo_container} >
-                        <div className={styles.logo_wrapper} >
-                            <PlatformLogo
-                                platformName={info.platformName}
-                                width={49}
-                            />
-                        </div>
-                        <div className={styles.logo_text}>Wallet Name</div>
-                    </div>
-                </div>
-                <div style={{ textAlign: 'center' }}>
-                    {/* <div className={styles.subtitle}></div> */}
-                    <span className={styles.sm_txt}><br />Buy with any card and earn up to <span className={styles.cashback_amount}>{formatCashback(+info.maxCashback, info.cashbackSymbol, info.cashbackCurrency)}</span> in {info.cryptoSymbols[0]}
-                        {/* <CryptoSymbolSelect
+            <AnimatePresence>
+                {
+                    !optOutOpen ?
+                        <motion.div
+                            key="main"
+                            className={styles.container}
+                            initial={{ x: 0, opacity: 1 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            exit={{ x: "-100%", opacity: 0 }}
+                            transition={{ duration: .2, ease: "easeInOut" }}
+                        >
+                            {walletAddress ?
+                                <div className={styles.top_container}>
+                                    <div className={styles.wallet_container}>
+                                        <span className={styles.wallet}>{splitWordMaxFive(walletAddress)}</span>
+                                    </div>
+                                    <SwitchBtn
+                                        callback={() => setStatus('switch')}
+                                    />
+                                </div>
+                                :
+                                <div className={styles.wallet_spacer} />
+                            }
+                            <div className={styles.details}>
+                                <div style={{ display: 'flex', gap: '8px', alignItems: 'start' }}>
+                                    <div className={styles.logo_container} >
+                                        <div className={styles.logo_wrapper} >
+                                            <img
+                                                src={iconUrl}
+                                                className={styles.logo}
+                                                alt={`${name}-website-icon`}
+                                                width={76}
+                                                height={76}
+                                            />
+                                        </div>
+                                        <div className={styles.logo_text}>{name}</div>
+                                    </div>
+                                    <img src="/icons/plus-sign.svg" alt="plus-sign" style={{ marginTop: '32px' }} />
+                                    <div className={styles.logo_container} >
+                                        <div className={styles.logo_wrapper} >
+                                            <PlatformLogo
+                                                platformName={info.platformName}
+                                                width={49}
+                                            />
+                                        </div>
+                                        <div className={styles.logo_text}>{toCapital(info.platformName)} wallet</div>
+                                    </div>
+                                </div>
+                                <div style={{ textAlign: 'center' }}>
+                                    {/* <div className={styles.subtitle}></div> */}
+                                    <span className={styles.sm_txt}><br />Buy with any card and earn up to <span className={styles.cashback_amount}>{formatCashback(+info.maxCashback, info.cashbackSymbol, info.cashbackCurrency)}</span> in {info.cryptoSymbols[0]}
+                                        {/* <CryptoSymbolSelect
                         options={info.cryptoSymbols}
                         select={tokenSymbol}
                         set={setTokenSymbol}
                     /> */}
-                    </span>
-                </div>
-            </div>
-            <div className={styles.action_container}>
-                <button
-                    onClick={handleFirstClick}
-                    className={styles.btn}
-                    disabled={status !== 'idle'}
-                >
-                    {toCaseString("Activate", textMode)}
-                </button>
-                <div className={styles.btns_container}>
-                    <button
-                        className={styles.action_btn}
-                        onClick={() => setOptOutOpen(true)}
-                    >
-                        {toCaseString("Opt out", textMode)}
-                    </button>
-                    <button
-                        className={styles.action_btn}
-                        onClick={async () => {
-                            await sendGaEvent('popup_close', {
-                                category: 'user_action',
-                                action: 'click',
-                                details: 'extension'
-                            })
-                            closeFn()
-                        }}
-                    >
-                        {toCaseString("Cancel", textMode)}
-                    </button>
-                </div>
-                <div style={{ textAlign: 'center', color: 'white', textWrap: 'nowrap', fontSize: '14px', lineHeight: '22px' }}>No extra steps required - just shop and get {info.cryptoSymbols[0]} in your wallet</div>
-            </div>
-            <LoadingOverlay
-                open={['waiting', 'switch'].includes(status)}
-            />
-            <OptOut
-                open={optOutOpen}
-                onClose={() => setOptOutOpen(false)}
-            />
-        </div >
+                                    </span>
+                                </div>
+                            </div>
+                            <div className={styles.action_container}>
+                                <button
+                                    onClick={handleFirstClick}
+                                    className={styles.btn}
+                                    disabled={status !== 'idle'}
+                                >
+                                    {toCaseString("Activate", textMode)}
+                                </button>
+                                <div className={styles.btns_container}>
+                                    <button
+                                        className={styles.action_btn}
+                                        onClick={() => setOptOutOpen(true)}
+                                    >
+                                        {toCaseString("Opt out", textMode)}
+                                    </button>
+                                    <button
+                                        className={styles.action_btn}
+                                        onClick={async () => {
+                                            await sendGaEvent('popup_close', {
+                                                category: 'user_action',
+                                                action: 'click',
+                                                details: 'extension'
+                                            })
+                                            closeFn()
+                                        }}
+                                    >
+                                        {toCaseString("Cancel", textMode)}
+                                    </button>
+                                </div>
+                                <div style={{ textAlign: 'center', color: 'white', textWrap: 'nowrap', fontSize: '14px', lineHeight: '22px' }}>No extra steps required - just shop and get {info.cryptoSymbols[0]} in your wallet</div>
+                            </div>
+                            <LoadingOverlay
+                                open={['waiting', 'switch'].includes(status)}
+                            />
+                        </motion.div>
+                        :
+                        <motion.div
+                            key="optout"
+                            initial={{ x: "100%", opacity: 0 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            exit={{ x: "100%", opacity: 0 }}
+                            transition={{ duration: .2, ease: "easeInOut" }}
+                            className={styles.container}
+                        >
+                            <OptOut
+                                onClose={() => setOptOutOpen(false)}
+                            />
+                        </motion.div>
+                }
+            </AnimatePresence>
+        </>
     )
 }
 
--- iframe-frontend/src/components/OptOut/OptOut.tsx
+++ iframe-frontend/src/components/OptOut/OptOut.tsx
@@ -1,32 +1,89 @@
 import styles from './styles.module.css'
 import { sendMessage, ACTIONS } from '../../utils/sendMessage';
-import { useState, useRef, useEffect, useCallback } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { useState, useCallback } from 'react';
 import { useGoogleAnalytics } from '../../hooks/useGoogleAnalytics';
+import { useRouteLoaderData } from 'react-router-dom';
+import toCapital from '../../utils/toCapital';
+import toCaseString from '../../utils/toCaseString';
 
-interface Props {
-    onClose: () => void;
-    open: boolean
+interface Option {
+    label: string
+    value: number | string | boolean
 }
 
-const options = [
-    { label: '24 hours', time: 24 * 60 * 60 * 1000 },
-    { label: '7 days', time: 7 * 24 * 60 * 60 * 1000 },
-    { label: '30 days', time: 30 * 24 * 60 * 60 * 1000 },
-    { label: 'forever', time: 999999999999999 },
+const websiteOptions: Option[] = [
+    { label: 'For this website', value: 0 },
+    { label: 'For all websites', value: 1 }
+]
+
+const durationOptions: Option[] = [
+    { label: '24 hours', value: 24 * 60 * 60 * 1000 },
+    { label: '30 days', value: 30 * 24 * 60 * 60 * 1000 },
+    { label: 'forever', value: 999999999999999 },
 ]
 
 const dict = {
     '24 hours': '24Hours',
-    '7 days': '7Days',
     '30 days': '30Days',
     'forever': 'forever'
 }
 
-const OptOut = ({ open, onClose }: Props) => {
+interface RadioGroupProps {
+    title: string
+    options: Option[]
+    onChange: (option: Option) => void
+    defaultOption?: Option
+}
+
+const RadioGroup = ({ title, options, onChange, defaultOption }: RadioGroupProps) => {
+    const [checked, setChecked] = useState<Option | null>(defaultOption || null)
+
+    return (
+        <div className={styles.radio_group}>
+            <div className={styles.radio_group_title}>{title}</div>
+            <div className={styles.radio_container}>
+                {options.map((option) => (
+                    <span
+                        key={option.label}
+                        style={{ display: 'flex', gap: '8px', alignItems: 'center', color: 'white' }}
+                    >
+                        <input
+                            className={styles.radio}
+                            onChange={() => {
+                                setChecked(option)
+                                onChange(option)
+                            }}
+                            type='radio'
+                            name={title}
+                            id={option.label}
+                            value={option.label}
+                            checked={checked?.label === option.label}
+                        />
+                        <label className={styles.label} htmlFor={option.label}>{option.label}</label>
+                    </span>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+interface Selection {
+    websites: Option
+    duration: Option
+}
+
+interface Props {
+    onClose: () => void;
+}
+
+const OptOut = ({ onClose }: Props) => {
+    const { cryptoSymbols, platformName, textMode } = useRouteLoaderData('root') as LoaderData
     const { sendGaEvent } = useGoogleAnalytics()
     const [isOpted, setIsOpted] = useState(false)
-    const popupRef = useRef<HTMLDivElement>(null);
+    const [selection, setSelection] = useState({
+        websites: websiteOptions[0],
+        duration: durationOptions[0]
+    })
 
     const handleClose = useCallback((): void => {
         if (isOpted) {
@@ -36,77 +93,56 @@ const OptOut = ({ open, onClose }: Props) => {
         }
     }, [isOpted, onClose])
 
-    useEffect(() => {
-        function handleClickOutside(event: MouseEvent) {
-            if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
-                handleClose();
-            }
-        }
-
-        document.addEventListener('mousedown', handleClickOutside);
-
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [onClose, isOpted, handleClose]);
-
-    const handleOptOut = (time: number, label: string) => {
-
-        sendMessage({ action: ACTIONS.OPT_OUT, time, key: dict[label as keyof typeof dict] })
+    const handleOptOut = (selection: Selection) => {
+        const { websites, duration } = selection
+        console.log(websites, duration)
+        sendMessage({ action: ACTIONS.OPT_OUT, time: +duration.value, key: dict[duration.label as keyof typeof dict] })
         setIsOpted(true)
-        sendGaEvent('opt_out', {
-            category: 'user_action',
-            action: 'click',
-            details: label
-        })
+        // sendGaEvent('opt_out', {
+        //     category: 'user_action',
+        //     action: 'click',
+        //     details: label
+        // })
     }
 
     return (
-        <AnimatePresence>
-            {open ?
-                <motion.div
-                    transition={{ ease: 'easeInOut' }}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    className={styles.overlay}>
-                    <motion.div
-                        ref={popupRef}
-                        transition={{ ease: 'easeInOut' }}
-                        initial={{ y: '100px' }}
-                        animate={{ y: '0' }}
-                        exit={{ y: '100px' }}
-                        className={styles.card}>
-                        {!isOpted ?
-                            <>
-                                <div className={styles.title}>Turn off Cashback offers for</div>
-                                <div className={styles.container}>
-                                    {options.map((option) => (
-                                        <button
-                                            key={option.label}
-                                            className={styles.btn}
-                                            onClick={() => handleOptOut(option.time, option.label)}
-                                        >
-                                            {option.label}
-                                        </button>
-                                    ))}
-                                </div>
-                            </>
-                            :
-                            <div className={styles.message}>Your request to turn off offers has been received. You can reactivate them anytime in settings.</div>
-                        }
-                        <button
-                            className={styles.close_btn}
-                            onClick={handleClose}
-                        >
-                            {isOpted ? 'Close' : 'Cancel'}
-                        </button>
-                    </motion.div >
-                </motion.div>
+        <div
+            className={styles.container}>
+            {!isOpted ?
+                <div className={styles.card}>
+                    <div className={styles.title}>Turn off Cashback offers</div>
+                    <div className={styles.description}>
+                        With {toCapital(platformName)}’s cashback you earn {cryptoSymbols[0]}, right in<br />your wallet, on everyday purchases
+                    </div>
+                    <RadioGroup
+                        options={websiteOptions}
+                        title={`Where do you want to turn off ${cryptoSymbols[0]} cashback?`}
+                        onChange={(option => setSelection({ ...selection, websites: option }))}
+                        defaultOption={websiteOptions[0]}
+                    />
+                    <RadioGroup
+                        options={durationOptions}
+                        title={`Choose when to hide cashback offers`}
+                        onChange={(option => setSelection({ ...selection, duration: option }))}
+                        defaultOption={durationOptions[0]}
+                    />
+                </div>
                 :
-                null
+                <div className={styles.message}>Your request to turn off offers has been received. You can reactivate them anytime in settings.</div>
             }
-        </AnimatePresence>
+            <button
+                className={`${styles.btn} ${styles.apply_btn}`}
+                onClick={handleOptOut}
+            >
+                {toCaseString('Apply', textMode)}
+            </button>
+            <button
+                className={`${styles.btn} ${styles.close_btn}`}
+                onClick={handleClose}
+            >
+                {toCaseString('Back to activation', textMode)}
+            </button>
+        </div >
     )
 }
 
