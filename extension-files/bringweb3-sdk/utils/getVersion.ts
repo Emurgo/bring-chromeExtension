9301d95ed9cdb6a7e3559dc904f9e9cdbc88a68c
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2025-10-08 21:53:26 +0300

    version returning fix for local non-standard fork versions

--- extension-files/bringweb3-sdk/utils/getVersion.ts
+++ extension-files/bringweb3-sdk/utils/getVersion.ts
@@ -2,7 +2,7 @@ import pJson from '../package.json'
 import { ENV_VERSION } from './config'
 
 const getVersion = (): string => {
-    return ENV_VERSION || pJson.version
+    return (ENV_VERSION || pJson.version).split('-')[0] as string
 }
 
 export default getVersion
